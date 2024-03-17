175299c845baf8cd8d12c95487ac12daf953bda6
EasChen70 <eason_chen@uri.edu>
2024-03-17 17:34:01 -0400

    fetch data from exchange

--- tradingbot.py
+++ tradingbot.py
@@ -1,7 +1,14 @@
 import ccxt 
 from config import API_KEY, SECRET_KEY
 
-exchange = ccxt.coinbase({
+exchange = ccxt.binanceus({
+    'enableRateLimit': True,
     'apiKey': API_KEY,
-    'secretKey': SECRET_KEY
+    'secret': SECRET_KEY
 })
+
+balance = exchange.fetch_balance()
+
+for currency, amount in balance['total'].items():
+    if amount >= 0:
+        print(f"{currency}: {amount}")
